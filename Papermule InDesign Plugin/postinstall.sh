006f3a53eb77b0a2ef12dc8732d9ee50f6e81733
pwalker1485 <pwalker1485@icloud.com>
2020-05-26 16:18:47 +0100

    moved file deletions into cleanUp function

--- Papermule InDesign Plugin/postinstall.sh
+++ Papermule InDesign Plugin/postinstall.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 ########################################################################
-#               Install Papermule Adobe InDesign plugin                #
-#                   Written by Phil Walker Jan 2020                    #
-###################### postinstall script ##############################
+#              Install Papermule Adobe InDesign plugin                 #
+#                  Written by Phil Walker Jan 2020                     #
+######################## postinstall script ############################
 ########################################################################
 
 ########################################################################
 #                            Variables                                 #
 ########################################################################
 
-#Find which versions of InDesign are installed
+# Find which versions of InDesign are installed
 adobeInDesign=$(find /Applications/Adobe\ InDesign*/Scripts/startup\ scripts/ -type d -maxdepth 0 > /tmp/InDesignInstalls.txt)
-#Script temp location
+# Script temp location
 startupScript="/usr/local/Papermule/PapermuleIDCS4XTLSupportV1.jsx"
 
 ########################################################################
@@ -21,8 +21,10 @@ startupScript="/usr/local/Papermule/PapermuleIDCS4XTLSupportV1.jsx"
 
 function cleanUp ()
 {
-#Clean up temporary files and directories
-if [[ ! -f "/tmp/InDesignInstalls.txt" ]] && [[ ! -d "/usr/local/Papermule" ]]; then
+# Clean up temporary files and directories
+rm -f "/tmp/InDesignInstalls.txt"
+rm -rf "/usr/local/Papermule/"
+if [[ ! -f "/tmp/InDesignInstalls.txt" ]] && [[ ! -d "/usr/local/Papermule/" ]]; then
   echo "All temporary files and directories removed"
 else
   echo "Clean up failed, manual clean up required"
@@ -33,13 +35,10 @@ fi
 #                         Script starts here                           #
 ########################################################################
 
-while read line; do
-cp -pf "$startupScript" "$line"
-echo "Copied Papermule script to $line..."
+while IFS= read -r line; do
+    cp -p -f "$startupScript" "$line"
+    echo "Copied Papermule script to $line..."
 done < /tmp/InDesignInstalls.txt
-
-rm -f /tmp/InDesignInstalls.txt
-rm -rf /usr/local/Papermule
 
 cleanUp
 
