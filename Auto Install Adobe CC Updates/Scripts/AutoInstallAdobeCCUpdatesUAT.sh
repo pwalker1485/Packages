c15c936cd0ee6dbc14b24de791debf594d30e78a
pwalker1485 <pwalker1485@icloud.com>
2020-05-26 10:29:29 +0100

    cleaned up - all variable usage double quoted etc

--- Auto Install Adobe CC Updates/Scripts/AutoInstallAdobeCCUpdatesUAT.sh
+++ Auto Install Adobe CC Updates/Scripts/AutoInstallAdobeCCUpdatesUAT.sh
@@ -24,7 +24,7 @@ function checkConnectivity ()
 {
 #Check if the Mac has internet connectivity
 ping -q -c 1 -W 1 www.apple.com >/dev/null 2>&1
-if [[ $? == "0" ]]; then
+if [[ "$?" == "0" ]]; then
     echo "Internet connectivity detected"
 else
     echo "Internet connectivity not detected, exiting"
@@ -37,7 +37,7 @@ function checkPower ()
 ##Check if the Mac is on ac power or has over 50% battery available
 pwrAdapter=$(/usr/bin/pmset -g ps)
 batteryPercentage=$(/usr/bin/pmset -g ps | grep -i "InternalBattery" | awk '{print $3}' | cut -c1-3 | sed 's/%//g')
-if [[ ${pwrAdapter} =~ "AC Power" ]] || [[ ${batteryPercentage} -ge "50" ]]; then
+if [[ "$pwrAdapter" =~ "AC Power" ]] || [[ "$batteryPercentage" -ge "50" ]]; then
 	echo "Sufficient power detected"
 else
 	echo "AC power not detected, exiting"
@@ -59,9 +59,9 @@ else
     checkPower
 
     #Remove previous log
-    if [[ -f $rumLog ]]; then
-        rm -f $rumLog 2>/dev/null
-        if [[ ! -f $rumLog ]]; then
+    if [[ -f "$rumLog" ]]; then
+        rm -f "$rumLog" 2>/dev/null
+        if [[ ! -f "$rumLog" ]]; then
             echo "Previous temp log file deleted successfully"
         fi
     else
@@ -69,16 +69,16 @@ else
     fi
 
     #Create log file, check for available updates and output results to the log
-    touch $rumLog
-    $rumBinary --action=list > $rumLog
+    touch "$rumLog"
+    "$rumBinary" --action=list > "$rumLog"
 
     #Check if any updates are required
-    updatesCheck=$(cat $rumLog)
+    updatesCheck=$(cat "$rumLog")
     if [[ "$updatesCheck" =~ "Following Updates are applicable" ]]; then
         echo "Updates available"
         echo "Installing all available updates..."
         #Install all available updates and output result to the log
-        $rumBinary --action=install >> $rumLogUAT
+        "$rumBinary" --action=install >> "$rumLogUAT"
         echo "Successful update installations detailed in the UAT log"
     else
         echo "No available updates found"
