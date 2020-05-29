3b9695a8cfc3eaf970411d0455f6f99b3464eb52
pwalker1485 <pwalker1485@icloud.com>
2020-05-29 11:13:47 +0100

    updated with OS ver check for UT package and changed helpers

--- Universal Audio/UAD/UADSoundcard_SoftwareFirmwareandPlugins_Postinstall.sh
+++ Universal Audio/UAD/UADSoundcard_SoftwareFirmwareandPlugins_Postinstall.sh
@@ -5,11 +5,29 @@
 #################### Written by Phil Walker Jan 2020 ###################
 ########################################################################
 
+# Edit May 2020
+
 ########################################################################
 #                            Variables                                 #
 ########################################################################
 
-pkgReceipt=$(pkgutil --pkgs | grep "com.uaudio.installer.apollo")
+# Package receipt
+pkgReceipt=$(pkgutil --pkgs | grep "ukuadsoftwarefirmwareandplugins9.12.1")
+# OS Version Full and Short
+osFull=$(sw_vers -productVersion)
+osShort=$(sw_vers -productVersion | awk -F. '{print $2}')
+# Mac model full name
+macModelFull=$(system_profiler SPHardwareDataType | grep "Model Name" | sed 's/Model Name: //' | xargs)
+# Jamf Helper
+jamfHelper="/Library/Application Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper"
+# Helper problem icon 
+helperProblemIcon="/System/Library/CoreServices/Problem Reporter.app/Contents/Resources/ProblemReporter.icns"
+# Helper SS icon
+helperSelfServiceIcon="/Library/Application Support/JAMF/bin/Management Action.app/Contents/Resources/Self Service.icns"
+# Helper title
+helperTitle="Message from Bauer IT"
+# Helper heading
+helperHeading="UAD Soundcard Software, Firmware and Plugins"
 
 ########################################################################
 #                            Functions                                 #
@@ -18,7 +36,8 @@ pkgReceipt=$(pkgutil --pkgs | grep "com.uaudio.installer.apollo")
 function jamfHelperFailed ()
 {
 #jamf Helper to advise that the install has failed
-/Library/Application\ Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper -windowType utility -icon /System/Library/CoreServices/Problem\ Reporter.app/Contents/Resources/ProblemReporter.icns -title "Message from Bauer IT" -heading "UAD Soundcard Software, Firmware and Plugins" -description "Installation failed!
+"$jamfHelper" -windowType utility -icon "$helperProblemIcon" -title "$helperTitle" \
+-heading "$helperHeading" -description "Installation failed❗️
 
 Please contact the IT Service Desk on 0345 058 4444 for assistance" -timeout 60 -button1 "Ok" -defaultButton "1"
 }
@@ -26,23 +45,39 @@ Please contact the IT Service Desk on 0345 058 4444 for assistance" -timeout 60 
 function jamfHelperUpdateComplete ()
 {
 #Show a message via Jamf Helper that the install has completed
-/Library/Application\ Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper -windowType utility -icon /Library/Application\ Support/JAMF/bin/Management\ Action.app/Contents/Resources/Self\ Service.icns -title "Message from Bauer IT" -heading "UAD Soundcard Software, Firmware and Plugins" -description "Installation complete
-Your Mac will now be rebooted" -alignDescription natural -timeout 15 -button1 "Ok" -defaultButton "1"
+"$jamfHelper" -windowType utility -icon "$helperSelfServiceIcon" -title "$helperTitle" \
+-heading "$helperHeading" -description "Installation complete ✅" \
+-alignDescription natural -timeout 15 -button1 "Ok" -defaultButton "1"
 }
 
 ########################################################################
 #                         Script starts here                           #
 ########################################################################
 
+# Check OS version and install relevant user preference package (User Template)
+# Different packages due to the directory moving to /Library/User Template/ in Catalina
+echo "Checking User Template package requirements..."
+if [[ "$osShort" -le "14" ]]; then
+    echo "${macModelFull} running macOS ${osFull}"
+    /usr/local/jamf/bin/jamf policy -event uad_preferences_pre_catalina
+elif [[ "$osShort" -ge "15" ]]; then
+    echo "${macModelFull} running macOS ${osFull}"
+    /usr/local/jamf/bin/jamf policy -event uad_preferences
+else
+    echo "${macModelFull} running macOS ${osFull}, no User Template package requirement"
+fi
+
 #Kill the install in progess jamf Helper window
 killall jamfHelper
 
 if [[ "$pkgReceipt" != "" ]]; then
-  jamfHelperUpdateComplete
+    # Install was successful
+    jamfHelperUpdateComplete
 else
-  echo "UAD Soundcard Software, Firmware and Plugins package failed to install successfully"
-  jamfHelperFailed
-  exit 1
+    echo "UAD Soundcard Software, Firmware and Plugins package failed to install successfully"
+    # Advise user that the install has failed
+    jamfHelperFailed
+    exit 1
 fi
 
 exit 0
