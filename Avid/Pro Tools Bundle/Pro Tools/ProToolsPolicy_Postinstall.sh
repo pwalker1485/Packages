6452993c544c1933ad6be24647f4091fbfd3dd02
pwalker1485 <pwalker1485@icloud.com>
2020-05-29 11:12:55 +0100

    Moved OS ver check for user template package to start

--- Avid/Pro Tools Bundle/Pro Tools/ProToolsPolicy_Postinstall.sh
+++ Avid/Pro Tools Bundle/Pro Tools/ProToolsPolicy_Postinstall.sh
@@ -5,6 +5,8 @@
 #################### Written by Phil Walker Jan 2020 ###################
 ########################################################################
 
+# Edit May 2020
+
 ########################################################################
 #                            Variables                                 #
 ########################################################################
@@ -32,7 +34,7 @@ helperHeading="Pro Tools Bundle"
 function jamfHelperFailed ()
 {
 # jamf Helper to advise that Pro Tools failed to install
-"$jamfHelper" -windowType utility -icon "$helperIcon" -windowPosition ur -title "$helperTitle" \
+"$jamfHelper" -windowType utility -icon "$helperIcon" -title "$helperTitle" \
 -heading "$helperHeading" -description "Pro Tools failed to install successfully❗️
 
 No further software included in the Pro Tools bundle will be installed
@@ -44,20 +46,24 @@ Please contact the IT Service Desk on 0345 058 4444 for assistance" -timeout 60 
 #                         Script starts here                           #
 ########################################################################
 
+# Check OS version and install relevant user preference package (User Template)
+# Different packages due to the directory moving to /Library/User Template/ in Catalina
+echo "Checking User Template package requirements..."
+if [[ "$osShort" -le "14" ]]; then
+    echo "${macModelFull} running macOS ${osFull}"
+    /usr/local/jamf/bin/jamf policy -event pt_bundle_preferences_mojave
+elif [[ "$osShort" -ge "15" ]]; then
+    echo "${macModelFull} running macOS ${osFull}"
+    /usr/local/jamf/bin/jamf policy -event pt_bundle_preferences
+else
+    echo "${macModelFull} running macOS ${osFull}, no User Template package requirement"
+fi
+
 # Kill the install in progess jamf Helper window
 killall jamfHelper 2>/dev/null
 
 if [[ "$pkgReceipt" != "" ]]; then
-    echo "Checking User Template package requirements..."
-        if [[ "$osShort" -eq "14" ]]; then
-            echo "${macModelFull} running macOS ${osFull}"
-            /usr/local/jamf/bin/jamf policy -event pt_bundle_preferences_mojave
-        elif [[ "$osShort" -ge "15" ]]; then
-            echo "${macModelFull} running macOS ${osFull}"
-            /usr/local/jamf/bin/jamf policy -event pt_bundle_preferences
-        else
-            echo "${macModelFull} running macOS ${osFull}, no User Template package requirement"
-        fi
+    # Install was successful, call the next policy
     /usr/local/bin/jamf policy -event pt_bundle_iLokManager
 else
     echo "Pro Tools package failed to install successfully"
