233f57d4321f2de4c150211a3a156464b5bb2809
pwalker1485 <pwalker1485@icloud.com>
2021-02-05 00:06:56 +0000

    Removed redundant checks

--- SpryLabs Purple InDesign Extension/Scripts/postinstall.sh
+++ SpryLabs Purple InDesign Extension/Scripts/postinstall.sh
@@ -4,18 +4,18 @@
 #           Install Purple Export Extension for InDesign CC            #
 ################## Written by Phil Walker Sept 2019 ####################
 ########################################################################
-# Edit June 2020
+# Edit Feb 2021
 
 ########################################################################
 #                            Variables                                 #
 ########################################################################
 
+# Temp directory
+tempDir="/private/var/tmp/PurpleExtension"
 # Temp location of Extension Manager CLI
-exManCLI="/private/var/tmp/PurpleExtension/ExManCmd_mac/Contents/MacOS/ExManCmd"
-# Check current status of Purple Export Extension for InDesign CC
-extensionStatus=$("$exManCLI" --list all | grep "Purple InDesign Extension")
+exManCLI="${tempDir}/ExManCmdMac/Contents/MacOS/ExManCmd"
 # Temp location of the Purple Export Extension for InDesign CC 
-tempExtensionPath="/private/var/tmp/PurpleExtension/CC/PurpleExtension-1.4.9.zxp"
+tempExtensionPath="${tempDir}/PurpleExtension-1.4.9.zxp"
 
 ########################################################################
 #                            Functions                                 #
@@ -24,9 +24,9 @@ tempExtensionPath="/private/var/tmp/PurpleExtension/CC/PurpleExtension-1.4.9.zxp
 function cleanUp ()
 {
 # Clean up installation files
-if [[ -d "/private/var/tmp/PurpleExtension" ]]; then
-    rm -rf "/private/var/tmp/PurpleExtension"
-    if [[ ! -d "/private/var/tmp/PurpleExtension" ]]; then
+if [[ -d "$tempDir" ]]; then
+    rm -rf "$tempDir"
+    if [[ ! -d "$tempDir" ]]; then
         echo "Temp install files deleted successfully"
     else
         echo "Failed to clean-up temp files, manual clean-up required"
@@ -38,33 +38,22 @@ fi
 #                         Script starts here                           #
 ########################################################################
 
-# Check for previous version installed
-if [[ "$extensionStatus" != "" ]]; then
-    # Remove previous version
-    echo "Purple Export Extension for InDesign CC found"
-    "$exManCLI" --remove "Purple InDesign Extension"
-    if [[ "$("$exManCLI" --list all | grep "Purple InDesign Extension")" != "" ]]; then
-        echo "Purple Export Extension for InDesign CC removal failed!"
-        echo "Continuing anyway..."
-    fi
-else
-    echo "Purple Export Extension for InDesign CC not found, nothing to remove"
-fi
-
+# Remove previous version
+echo "Removing any previous version of the Purple Export Extension for InDesign..."
+"$exManCLI" --remove "Purple InDesign Extension" >/dev/null 2>&1
 # Install Purple Extension
-echo "Installing Purple Export Extension for InDesign CC..."
+echo "Installing latest Purple Export Extension for InDesign..."
 "$exManCLI" --install "$tempExtensionPath"
-
-# Check the installation was successful
-if [[ "$("$exManCLI" --list all | grep "Purple InDesign Extension")" != "" ]]; then
-    echo "Purple Export Extension for InDesign CC installed successfully"
+# Check the install was successful
+commandResult="$?"
+if [[ "$commandResult" -eq "0" ]]; then
+    echo "Purple Export Extension for InDesign installed successfully"
     # Remove all temp files
     cleanUp
 else
-    echo "Purple Export Extension for InDesign CC installation FAILED!"
+    echo "Purple Export Extension for InDesign installation FAILED!"
     # Remove all temp files
     cleanUp
     exit 1
 fi
-
 exit 0
