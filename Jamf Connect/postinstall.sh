06ddf685fd9f0754c0dd7c583c05fad6ecdf8297
pwalker1485 <pwalker1485@icloud.com>
2021-02-25 21:38:56 +0000

    Added additional content for zero touch specifically

--- Jamf Connect/postinstall.sh
+++ Jamf Connect/postinstall.sh
@@ -59,7 +59,7 @@ if [[ -d "$tempDir" ]]; then
         exit 1
     fi
     # Install Jamf Connect Launch Agent
-        if [[ -e "${tempDir}/${launchAgentInstaller}" ]]; then
+    if [[ -e "${tempDir}/${launchAgentInstaller}" ]]; then
         echo "Installing Jamf Connect Launch Agent..."
         installer -pkg "${tempDir}/${launchAgentInstaller}" -target /
         agentInstallResult="$?"
@@ -85,4 +85,31 @@ else
 fi
 # Remove temp content
 cleanUp
+
+####################### For Zero Touch Only ############################
+# A slow network connection during enrolment could result in the end user
+# being presented with a standard macOS login window.
+# In this scenario, to make sure the end user is presented with the Jamf
+# Connect login window the loginwindow process must be killed.
+# The loginwindow process will only be killed if specific conditions are met
+
+# Wait until the AppleSetupDone file is created. Required for macOS Big Sur and later
+until [[ -f "/var/db/.AppleSetupDone" ]]; do
+    sleep 2
+done
+echo "AppleSetupDone file found"
+# Get the logged in user
+loggedInUser=$(stat -f %Su /dev/console)
+if [[ "$loggedInUser" == "_mbsetupuser" ]] || [[ "$loggedInUser" == "root" ]] || [[ "$loggedInUser" == "" ]]; then
+	# If Setup Assistant is running, exit and let macOS finish setup assistant and display the new Jamf Connect login screen
+    setupAssistantProcess=$(pgrep -l "Setup Assistant")
+    if [[ "$setupAssistantProcess" != "" ]]; then
+        echo "Setup Assistant still running, exiting..."
+        exit 0
+    else
+        # Kill the login window to reload it and show the Jamf Connect login window
+        echo "Killing the loginwindow process to load the Jamf Connect login window..."
+        /usr/bin/killall -9 loginwindow
+    fi
+fi
 exit 0
